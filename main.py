db72666c1373c40a045b9877ee2d9934b41f3cf2
kostyamorales <kostyamorales@gmail.com>
2020-11-19 16:21:26 +0300

    Fixed exception handling

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from pathlib import Path
 import json
 import requests
 from time import sleep
+import logging
+import sys
+
+logger = logging.getLogger()
 
 
 def parse_arguments():
@@ -24,6 +28,7 @@ def parse_arguments():
 
 
 def main():
+    logging.basicConfig(level='INFO', format='%(filename)s - %(asctime)s - %(levelname)s - %(message)s')
     args = parse_arguments()
     start_page = args.start_page
     end_page = args.end_page
@@ -32,6 +37,7 @@ def main():
     if args.dest_folder:
         Path(args.dest_folder).mkdir(parents=True, exist_ok=True)
         chdir(args.dest_folder)
+    attempt = 1
     for book_url in books_urls:
         try:
             book_num = book_url.split('b')[-1]
@@ -40,9 +46,14 @@ def main():
             book = get_book(book_url, book_html, response, args.skip_txt, args.skip_imgs)
             books.append(book)
         except requests.HTTPError:
-            continue
-        except ConnectionError:
+            logger.info('requests.HTTPError')
+        except ConnectionError as error:
+            if attempt > 3:
+                logger.info(error)
+                sys.exit()
+            logger.info(error)
             sleep(30)
+            attempt += 1
     if args.json_path:
         Path(args.json_path).mkdir(parents=True, exist_ok=True)
         chdir(args.json_path)
